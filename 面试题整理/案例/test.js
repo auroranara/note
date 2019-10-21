304bcc490b0863c1c884bdfc68df345e6e1e0ac4
zangyunchong <yczang@jingan-china.cn>
2019-10-21 15:08:52 +0800

    fix: update

--- 面试题整理/案例/test.js
+++ 面试题整理/案例/test.js
@@ -1,32 +1,12 @@
-function fn(arr) {
-  const len = arr.length
-  for (let i = len; i >= 2; i--) {
-    for (let j = 0; j <= i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
-      }
-    }
-  }
-  return arr
+
+const url = 'https://pic4.zhimg.com/50/v2-d59417f35c710adfba8cbce8951c08d0_400x224.jpg'
+let image = new Image()
+image.src = url
+image.onload = function () {
+  const dom = document.createElement('canvas')
+  const canvas = dom.getContext('2d')
+  canvas.drawImage(image)
+  const newUrl = canvas.toDataURL()
+  console.log('newUrl', newUrl);
+
 }
-
-function fn1(arr) {
-  let i = arr.length - 1
-
-  while (i > 0) {
-    let pos = 0
-    for (let j = 0; j < i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        pos = j
-        const temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
-      }
-    }
-    i = pos
-  }
-  return arr
-}
-
-console.log(fn([2, 1, 7, 4, 5, 6, 3]));
-
